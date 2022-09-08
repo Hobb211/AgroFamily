b3653acd1222ebd6c119a504381e7c9aba9de1f9
Rosencrantz201 <vicmimora@live.com>
2022-09-08 00:43:00 -0300

    add login interface

--- AgroFamily/AgroFamily.xaml.cs
+++ AgroFamily/AgroFamily.xaml.cs
@@ -24,5 +24,10 @@ namespace AgroFamily
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
