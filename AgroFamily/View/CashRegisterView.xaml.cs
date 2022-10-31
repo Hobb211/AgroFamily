0e51a75ce2a9ff5a161594f1b66328db67f152f9
Cristian D. Zuñiga <cristian.zuniga@alumnos.ucn.cl>
2022-10-30 22:37:38 -0300

    vista caja

---
+++ AgroFamily/View/CashRegisterView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using AgroFamily.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AgroFamily.View
+{
+    /// <summary>
+    /// Lógica de interacción para CashRegisterView.xaml
+    /// </summary>
+    public partial class CashRegisterView : UserControl
+    {
+        public CashRegisterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
