8b63bae24cfd36ec5a9d41f99f275375e065ba9d
Sebaledex <sebastian.ledezma@alumnos.ucn.cl>
2022-11-05 12:13:55 -0300

    SaleView Complete

---
+++ AgroFamily/Model/SaleModel.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroFamily.Model
+{
+    public class SaleModel
+    {
+        public string id_vendedor { get; set; }
+        public string nombre_producto { get; set; }
+
+        public int[] id_producto { get; set; }
+
+        public string fecha_Venta { get; set; }
+
+        public int[] cantidad { get; set; }
+
+        public int[] precio_producto { get; set; }
+    }
+}
+
---
+++ AgroFamily/Repositories/SaleRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AgroFamily.Model;
+using SQLite;
+
+namespace AgroFamily.Repositories
+{
+    public class SaleRepository : RepositoryBase, ISaleRepository
+    {
+        public void Add(SaleModel saleModel)
+        {
+            using (SQLiteConnection connection = GetConnection())
+            {
+                connection.CreateTable<SaleModel>();
+                connection.Insert(saleModel);
+            }
+        }
+
+        public void agregar(SaleModel saleetModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(SaleModel saleModel)
+        {
+            using (SQLiteConnection connection = GetConnection())
+            {
+                connection.Update(saleModel);
+            }
+        }
+
+        public void eliminar(SaleModel saletModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void establecerCambios(SaleModel saletModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SaleModel> GetAll()
+        {
+            using (SQLiteConnection connection = GetConnection())
+            {
+                int cant = connection.Query<SaleModel>("select * from SaleModel").Count();
+                SaleModel[] saleModel = new SaleModel[cant];
+                for (int i = 1; i <= cant; i++)
+                {
+                    saleModel[i] = connection.Find<SaleModel>(i);
+                }
+                return saleModel;
+            }
+        }
+
+        public SaleModel GetById(int id)
+        {
+            using (SQLiteConnection connection = GetConnection())
+            {
+                return connection.Find<SaleModel>(id);
+            }
+        }
+
+        public void mostrarInventario(SaleModel saletModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
