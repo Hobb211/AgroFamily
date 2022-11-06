3d357c2f9c0e2860152fe861b5810e68bdaad7f3
tam210 <ossandon.t20@gmail.com>
2022-11-06 19:18:24 -0300

    Lógica del objeto Artículo implementada + Vista del estado de negocio

---
+++ AgroFamily/Model/ArticleModel.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroFamily.Model
+{
+    public class ArticleModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
