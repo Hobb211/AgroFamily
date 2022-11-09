c0ac59b6e39e9ce932adf117559a017a10bb51a1
tam210 <ossandon.t20@gmail.com>
2022-11-09 20:54:08 -0300

    inventario

--- AgroFamily/ViewModel/InventoryViewModel.cs
+++ AgroFamily/ViewModel/InventoryViewModel.cs
@@ -13,11 +13,21 @@ namespace AgroFamily.ViewModel
     public class InventoryViewModel : ViewModelBase
     {
         private ObservableCollection<ArticleModel> _articles;
+        private ObservableCollection<ProductModel> _products;
+        private ObservableCollection<SuppliesModel> _supplies;
         public ObservableCollection<ArticleModel> Articles { get => _articles; set { _articles = value; OnPropertyChanged(nameof(Articles)); } }
+        public ObservableCollection<ProductModel> Products { get => _products; set { _products = value; OnPropertyChanged(nameof(Products)); } }
+        public ObservableCollection<SuppliesModel> Supplies { get => _supplies; set { _supplies = value; OnPropertyChanged(nameof(Supplies)); } }
         public InventoryViewModel()
         {
             //IArticleRepository articleRepository = new ArticleRepository();
             //Articles = articleRepository.GetByAll();
+
+            IProductRepository productRepository = new ProductRepository();
+            ISuppliesRepository suppliesRepository = new SuppliesRepository();  
+
+            Articles = suppliesRepository.GetByAllArticles();
+            Articles = new ObservableCollection<ArticleModel>(suppliesRepository.GetByAllArticles().Concat(productRepository.GetByAllArticles()));
         }
     }
 }
