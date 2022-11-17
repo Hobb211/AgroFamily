f7dae9b4e3b1d740828c53810e09d142d48f2eb5
Cristian D. Zuñiga <cristian.zuniga@alumnos.ucn.cl>
2022-11-17 02:37:12 -0300

    Avance historial de ventas

--- AgroFamily/View/SalesHistoryView.xaml.cs
+++ AgroFamily/View/SalesHistoryView.xaml.cs
@@ -24,5 +24,10 @@ namespace AgroFamily.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
--- AgroFamily/ViewModel/SaleHistoryViewModel.cs
+++ AgroFamily/ViewModel/SaleHistoryViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using AgroFamily.Model;
+using AgroFamily.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -15,19 +16,21 @@ namespace AgroFamily.ViewModel
         //Fields
         private string _sellerID;
         private string _saleID;
-        private DateTime _startingDate;
-        private DateTime _endingDate;
+        private DateOnly _startingDate;
+        private DateOnly _endingDate;
+        private SaleModel _currentSale;
         private bool _onlyToday;
         private bool _dateRange;
-        public readonly ObservableCollection<SaleModel> _salesOfSearch;
+        public ObservableCollection<SaleModel> _salesOfSearch;
 
         //Properties
         public string SellerID { get => _sellerID; set { _sellerID = value; OnPropertyChanged(nameof(SellerID)); } }
         public string SaleID { get => _saleID; set { _saleID = value; OnPropertyChanged(nameof(SaleID)); } }
-        public DateTime StartingDate { get => _startingDate; set { _startingDate = value; OnPropertyChanged(nameof(StartingDate)); } }
-        public DateTime EndingDate { get => _endingDate; set { _endingDate = value; OnPropertyChanged(nameof(EndingDate)); } }
-        public bool OnlyToday { get => _onlyToday; set { _onlyToday = value; OnPropertyChanged(nameof(OnlyToday)); } }
-        public bool DateRange { get => _dateRange; set { _dateRange = value; OnPropertyChanged(nameof(DateRange)); } }
+        public DateOnly StartingDate { get => _startingDate; set { _startingDate = value; OnPropertyChanged(nameof(StartingDate)); } }
+        public DateOnly EndingDate { get => _endingDate; set { _endingDate = value; OnPropertyChanged(nameof(EndingDate)); } }
+        public SaleModel CurrentSale { get => _currentSale; set { _currentSale = value; OnPropertyChanged(nameof(CurrentSale)); } }
+        //public bool OnlyToday { get => _onlyToday; set { _onlyToday = value; OnPropertyChanged(nameof(OnlyToday)); } }
+        //public bool DateRange { get => _dateRange; set { _dateRange = value; OnPropertyChanged(nameof(DateRange)); } }
 
         //Commands
         public ICommand SearchSaleCommand { get; }
@@ -41,13 +44,48 @@ namespace AgroFamily.ViewModel
             SearchSaleCommand = new ViewModelCommand(ExecuteSearchSaleCommand, CanExecuteSearchSaleCommand);
         }
 
-        private void ExecuteSearchSaleCommand(object obj)
-        {
-            return;
-        }
         private bool CanExecuteSearchSaleCommand(object obj)
         {
-            return true;
+            bool validParameters = false;
+            bool validDates = false;
+
+            //Para poder buscar una venta los id de venta e id de vendedor deben ser no nulos
+            if (SellerID != null && SaleID != null)
+            {
+                validParameters = true;
+            }
+
+            //Respecto al rango de fechas se deben verificar las siguientes condiciones:
+            //Fecha final no sea anterior a la fecha de inicio
+            //Fecha de inicio y final no sea posterior al dia en curso
+            //Si ambas fechas son el dia actual
+            if (EndingDate.CompareTo(DateTime.Today) == 0 && StartingDate.CompareTo(DateTime.Today) == 0)
+            {
+                validDates = true;
+            }
+
+            else if(EndingDate.CompareTo(StartingDate) > 0 && EndingDate.CompareTo(DateTime.Today) < 0 && StartingDate.CompareTo(DateTime.Today) < 0)
+            {
+                validDates = true;
+            }
+
+            return validParameters && validDates;
+        }
+
+        private void ExecuteSearchSaleCommand(object obj)
+        {
+            IUserRepository userRepository = new UserRepository();
+            ISaleRepository saleRepository = new SaleRepository();
+            _salesOfSearch = saleRepository.GetByDateRange(StartingDate, EndingDate);
+            ObservableCollection<SaleModel> sales_aux = new ObservableCollection<SaleModel>();
+            foreach (SaleModel sale in _salesOfSearch)
+            {
+                if (sale.id_vendedor == SellerID)
+                {
+                    sales_aux.Add(sale);
+                }
+            }
+            _salesOfSearch = sales_aux;
         }
     }
 }
